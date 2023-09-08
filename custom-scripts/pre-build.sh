7d85e64fb7b67d0b8df0b150fa0ee77d6d6589d7
cilges <98175718+cilges@users.noreply.github.com>
2023-09-08 21:59:10 +0000

    webserver

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -2,3 +2,6 @@
 
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+
+cp $BASE_DIR/../custom-scripts/t1/webserver.py $BASE_DIR/target/usr/bin
+chmod +x $BASE_DIR/target/usr/bin/webserver.py
