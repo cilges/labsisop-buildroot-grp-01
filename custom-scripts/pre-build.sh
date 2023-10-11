eb504cb77e4981e562ff36f3c16f5ad3d9d2aa14
Guilherme R. Kuticoski <89421364+gKuticoski@users.noreply.github.com>
2023-10-11 21:25:26 +0000

    Tutorial 2.3

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+make -C $BASE_DIR/../modules/simple_driver/
+
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
 
