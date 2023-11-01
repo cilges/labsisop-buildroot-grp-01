b2b07925dc9e1361563ae4ba82e1d28e9cd62530
Guilherme R. Kuticoski <89421364+gKuticoski@users.noreply.github.com>
2023-11-01 20:58:27 +0000

    finsh T2

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -8,4 +8,4 @@ chmod +x $BASE_DIR/target/etc/init.d/S41network-config
 cp $BASE_DIR/../custom-scripts/S50hello $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S50hello
 
-make -C $BASE_DIR/../modules/sstf-iosched
+make -C $BASE_DIR/../modules/sstf
